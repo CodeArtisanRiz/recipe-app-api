0b69aec6d4694ca9cfd5cec83f387145dfa93048
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2023-12-30 21:49:13 +0530

    Email Validation lintFix

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -33,6 +33,9 @@ class ModelTests(TestCase):
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
-        """Test that raises a ValueError when creating a user without an email."""
+        """
+        Test that raises a ValueError when
+        creating a user without an email.
+        """
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
